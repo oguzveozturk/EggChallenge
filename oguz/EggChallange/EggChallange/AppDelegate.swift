5793dacfeae5280ec3aa1b835431e493738f5491
oguz <oguzveozturk@gmail.com>
2019-12-12 23:59:45 +0300

    initial project

---
+++ oguz/EggChallange/EggChallange/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  EggChallange
+//
+//  Created by Oguz on 12.12.2019.
+//  Copyright © 2019 Oguz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+ var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ oguz/EggChallange/EggChallange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EggChallange
+//
+//  Created by Oguz on 12.12.2019.
+//  Copyright © 2019 Oguz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
